8d4b580927cb9cd4a9406ffbf442bde777df6e18
quangnd <quangnd.edu@gmail.com>
2018-08-16 08:38:41 +0700

    Initial commit

---
+++ demo.php
@@ -0,0 +1,8 @@
+<?php
+  $array1 = array("quang" => "20", "minh" => "10");
+
+  foreach($array1 as $a => $a_value) {
+    echo "Key: " . $a . " and " . $a_value . "\n";
+    echo "Key $a and $a_value \n";
+  }
+?>
---
+++ notes.md
@@ -0,0 +1,7 @@
+1. Create Laravel project
+
+`composer create-project --prefer-dist laravel/laravel mylaravel`
+
+2. List make command by Artisan
+
+`php artisan list make`
