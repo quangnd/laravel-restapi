d3a84740c7b9eebee48ef614641f2d060e4fb6f8
Quang Nguyen <quangnd.edu@gmail.com>
2018-08-17 14:47:03 +0700

    Implement the operations for category

--- database/migrations/2018_08_14_150934_create_transactions_table.php
+++ database/migrations/2018_08_14_150934_create_transactions_table.php
@@ -20,7 +20,7 @@ class CreateTransactionsTable extends Migration
             $table->integer('product_id')->unsigned();
             $table->timestamps();
             $table->softDeletes();
-            
+
             $table->foreign('buyer_id')->references('id')->on('users');
             $table->foreign('product_id')->references('id')->on('products');
         });
