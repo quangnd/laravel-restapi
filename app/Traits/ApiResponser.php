fa13181211f5ca2b2daa4405b5f3853ad4849ece
Quang Nguyen <quangnd.edu@gmail.com>
2018-08-19 13:22:36 +0700

    Add sorting and filter based on query params

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -25,6 +25,8 @@ trait ApiResponser
 
         $transformer = $collection->first()->transformer;
 
+        $collection = $this->filterData($collection, $transformer);
+        $collection = $this->sortData($collection, $transformer);
         $collection = $this->transformData($collection, $transformer);
 
         return $this->successResponse($collection, $code);
@@ -42,6 +44,31 @@ trait ApiResponser
     protected function showMessage($message, $code = 200)
     {
         return $this->successResponse(['data' => $message], $code);
+    }
+
+    protected function filterData(Collection $collection, $transformer)
+    {
+        foreach (request()->query() as $query => $value) {
+            $attribute = $transformer::originalAttribute($query);
+
+            if (isset($attribute, $value)) {
+                $collection = $collection->where($attribute, $value);
+            }
+        }
+
+        return $collection;
+    }
+
+    protected function sortData(Collection $collection, $transformer)
+    {
+        if (request()->has('sort_by')) {
+            //transform into original attribute before do sorting
+            $attribute = $transformer::originalAttribute(request()->sort_by);
+
+            $collection = $collection->sortBy->{$attribute};
+        }
+
+        return $collection;
     }
 
     protected function transformData($data, $transformer)
