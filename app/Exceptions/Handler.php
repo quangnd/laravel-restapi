617b6db306752821de8abb4d5911bf592d3172e5
Quang Nguyen <quangnd.edu@gmail.com>
2018-08-19 15:23:34 +0700

    Generate auth with make:auth and preparing api for user authentication

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -109,6 +109,10 @@ class Handler extends ExceptionHandler
 
     protected function unauthenticated($request, AuthenticationException $exception)
     {
+        if ($this->isFrontend($request)) {
+            return redirect()->guest('login');
+        }
+
         return $this->errorResponse('Unauthenticated.', 401);
     }
 
@@ -117,6 +121,21 @@ class Handler extends ExceptionHandler
     {
         $errors = $e->validator->errors()->getMessages();
 
+        if ($this->isFrontend($request)) {
+            return $request->ajax() ? response()->json($errors, 422) : redirect()
+                ->back()
+                ->withInput($request->input())
+                ->withErrors($errors);
+        }
+
         return $this->errorResponse($errors, 422);
+    }
+
+    /**
+        Prevent Response Error with frontend
+     */
+    private function isFrontend($request)
+    {
+        return $request->acceptsHtml() && collect($request->route()->middleware())->contains('web');
     }
 }
